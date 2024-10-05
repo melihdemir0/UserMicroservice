b246def78987630c9d8537cace6989b863802493
melihdemir0 <melihdemirr0@gmail.com>
2024-10-05 17:03:41 +0300

    First commit

---
+++ src/test/java/tr/edu/ogu/ceng/Users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.ogu.ceng.Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
